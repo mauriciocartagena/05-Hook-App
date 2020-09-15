192af7d9be1ac9c1db8143dbfd76fb44d12d8b62
Mauricio Cartagena <mc973220@gmail.com>
2020-09-15 18:33:58 -0400

    formas de escuchar cambios de un input  con useEffect

--- src/components/01-useState/CounterApp.js
+++ src/components/01-useState/CounterApp.js
@@ -3,12 +3,14 @@ import './counter.css';
 
 export const CounterApp = () => {
 
-    const [{ counter1,counter2 }, setCounter] = useState({
+    const [state, setState] = useState({
         counter1:10,
-        counter2:20
+        counter2:20,
+        counter3:30,
+        counter4:40,
     });
+    const { counter1,counter2   } = state;
 
-    console.log(counter1);
     return (
         <>
             <h1>Counter1 { counter1 } </h1>
@@ -18,7 +20,10 @@ export const CounterApp = () => {
             <button 
                 className="btn btn-primary" 
                 onClick={ () => {
-                    setCounter( { counter1:counter1+1 ,counter2} ,  )      
+                    setState({
+                            ...state,
+                            counter1:counter1+1
+                    });      
                 }} 
                 >
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
     
-    <CounterApp />,
+    <SimpleForm />,
     
   document.getElementById('root')
 );
