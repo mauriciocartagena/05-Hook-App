b164564e7d7a33f131596cc0a894f941bfacc84c
Mauricio Cartagena <mc973220@gmail.com>
2020-09-16 16:26:23 -0400

    Formas de hacer que no tenga fugas de memoria con el UseRef

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -1,8 +1,19 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const useFetch = ( url ) => {
 
+    const isMounted = useRef(true);
+
     const [state, setState] = useState({ data: null, loading: true, error: null });
+
+    useEffect(() => {
+        
+        return() => {
+            
+            isMounted.current = false;
+        }
+    
+    }, [])
 
     useEffect(()=>{
 
@@ -11,11 +22,15 @@ export const useFetch = ( url ) => {
         fetch( url )
             .then( resp => resp.json() )
             .then( data =>{
-                setState({
-                    loading:false,
-                    error:null,
-                    data
-                })
+
+                if (isMounted.current) {
+                    setState({
+                        loading:false,
+                        error:null,
+                        data
+                    });
+                }
+
             });
 
     },[ url ]);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-import { FocusScreen } from './components/04-useRef/FocusScreen';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
     
-    <FocusScreen />,
+    <RealExampleRef />,
     
   document.getElementById('root')
 );
