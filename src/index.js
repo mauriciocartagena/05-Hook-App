27cc9afe5973d1da524088f882c43382486ee780
Mauricio Cartagena <mc973220@gmail.com>
2020-09-17 16:48:34 -0400

    introduccion a reducer

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Padre } from './components/07-tarea-memo/Padre';
-// import { CallBackHook } from './components/06-memos/CallBackHook';
-// import { MemoHook } from './components/06-memos/MemoHook';
-// import { Memorize } from './components/06-memos/Memorize';
-// import { Layout } from './components/05-useLayoutEffect/Layout';
-// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-// import { CounterApp } from './components/01-useState/CounterApp';
-// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-// import { FocusScreen } from './components/04-useRef/FocusScreen';
-// import { SimpleForm } from './components/02-useEffect/SimpleForm';
-// import { HookApp } from './HookApp';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Padre } from './components/07-tarea-memo/Padre';
+// // import { CallBackHook } from './components/06-memos/CallBackHook';
+// // import { MemoHook } from './components/06-memos/MemoHook';
+// // import { Memorize } from './components/06-memos/Memorize';
+// // import { Layout } from './components/05-useLayoutEffect/Layout';
+// // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// // import { CounterApp } from './components/01-useState/CounterApp';
+// // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// // import { FocusScreen } from './components/04-useRef/FocusScreen';
+// // import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// // import { HookApp } from './HookApp';
 
-ReactDOM.render(
+// ReactDOM.render(
     
-    <Padre />,
+//     <Padre />,
     
-  document.getElementById('root')
-);
+//   document.getElementById('root')
+// );
+
+import './components/08-useReducer/intro-reducer';
