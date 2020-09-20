ee58d3efe80f0abec648307c75c79034aa1039bb
Mauricio Cartagena <mc973220@gmail.com>
2020-09-20 17:20:53 -0400

    se hizo el test de increment decrement y reset de useCounter

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook,act } from "@testing-library/react-hooks";
 import '@testing-library/jest-dom';
 
 import { useMulti } from "../../components/hooks/useMulti";
@@ -18,6 +18,67 @@ describe('Pruebas de useCounter', () => {
 
 
     });
+
+    test('Debe de tener el counter en 100 ', () => {
+        
+        const { result } = renderHook( () => useMulti( 100 ) );
+        
+        expect( result.current.counter ).toBe(100);
+    });
+
+    test('Debe de incrementar el counter en 1', () => {
+
+        const { result } = renderHook( () => useMulti( 100 ) );
+
+        const { increment } = result.current;
+        
+       act( ()=>{
+
+        increment();
+
+       });
+       
+       const { counter } = result.current;
+
+       expect( counter ).toBe( 101 );
+
+    });
+    test('Debe de decrementar el counter en 1', () => {
+       
+        const { result } = renderHook( () => useMulti( 100 ) );
+
+        const { decrement } = result.current;
+
+        act(()=>{
+
+            decrement();
+
+        });
+        
+        const { counter } = result.current;
+
+        expect( counter ).toBe( 99 );
+        
+    });
+    test('Debe de resetear al valor enviado', () => {
+       
+        const { result } = renderHook( () => useMulti( 100 ) );
+
+        const {  decrement,reset } = result.current;
+
+        act(()=>{
+
+            decrement();
+            reset();
+
+        });
+        
+        const { counter } = result.current;
+
+        expect( counter ).toBe( 100 );
+        
+    });
+    
     
 
 });
