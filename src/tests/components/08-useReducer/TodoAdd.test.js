70e72b52a56317a6d7d153a3c7c566e151442685
Mauricio Cartagena <mc973220@gmail.com>
2020-09-22 15:48:34 -0400

    pruebas en TodoAdd

---
+++ src/tests/components/08-useReducer/TodoAdd.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+import { TodoAdd } from '../../../components/08-useReducer/TodoAdd';
+
+describe('Pruebas en <TodoAdd/>', () => {
+
+    const handleAddTodo = jest.fn();
+    
+
+    const wrapper = shallow(
+        <TodoAdd
+            handleAddTodo={ handleAddTodo }
+        />
+    )
+
+    test('Debe de mostrarse correctamente', () => {
+
+        expect( wrapper ).toMatchSnapshot();
+        
+    });
+
+    test('No debe de llamar handleAddTodo', () => {
+       
+        const formSubmit = wrapper.find('form').prop('onSubmit');
+
+        formSubmit({ preventDefault(){} });
+
+        expect( handleAddTodo ).toHaveBeenCalledTimes(0);
+        
+    });
+    test('debe de llamar la funcion handleAddTodo', () => {
+        // con un argumento //called
+
+        const value = 'Aprendiendo react';
+        wrapper.find('input').simulate('change',{
+            target:{
+                value,
+                name:'description'
+            }
+        });
+
+        const formSubmit = wrapper.find('form').prop('onSubmit');
+
+        formSubmit({ preventDefault(){} });
+
+        expect( handleAddTodo ).toHaveBeenCalledTimes(1);
+        expect( handleAddTodo ).toHaveBeenCalledWith(expect.any(Object));
+        expect( handleAddTodo ).toHaveBeenCalledWith({
+            
+            id:expect.any(Number),
+            desc:value,
+            done:false
+
+        });
+
+        expect( wrapper.find( 'input' ).prop('value') ).toBe('');
+
+        
+    });
+    
+    
+    
+
+});
